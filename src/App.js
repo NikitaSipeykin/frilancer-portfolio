54116d164fe887c07740bf4ae435da1152d9aec1
MacNikSipeykin <niksipeykin@gmail.com>
2025-08-13 00:53:14 +0800

    Add novigation functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import "./styles/main.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
 import Project from "./pages/Project";
-
-
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
   return (
     <div className="App">
-      
-      <Navbar />
+      <Router>
+        <ScrollToTop />
+        <Navbar />
 
-      {/* <Home /> */}
-      <Project />
-      {/* <Contacts /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
 
-      <Footer />
+        <Footer />
 
+      </Router>
     </div>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
-import vk from "./../../img/icons/vk.svg";
+import patreon from "./../../img/icons/vk.svg";
 import instagram from "./../../img/icons/instagram.svg";
-import twitter from "./../../img/icons/twitter.svg";
+import buymeacoffee from "./../../img/icons/twitter.svg";
 import gitHub from "./../../img/icons/gitHub.svg";
 import linkedIn from "./../../img/icons/linkedIn.svg";
 import "./style.css";
@@ -12,27 +12,27 @@ function Footer(){
           <div className="footer__wrapper">
             <ul className="social">
               <li className="social__item">
-                <a href="#!">
-                  <img src={vk} alt="Link" />
+                <a href="https://www.patreon.com/c/NikSipeikin" target="_blank" rel="noreferrer">
+                  <img src={patreon} alt="Link" />
                 </a>
               </li>
               <li className="social__item">
-                <a href="#!">
+                <a href="https://www.instagram.com/nikita.vitalevich" target="_blank" rel="noreferrer">
                   <img src={instagram} alt="Link" />
                 </a>
               </li>
               <li className="social__item">
-                <a href="#!">
-                  <img src={twitter} alt="Link" />
+                <a href="https://buymeacoffee.com/nikSipeikin" target="_blank" rel="noreferrer">
+                  <img src={buymeacoffee} alt="Link" />
                 </a>
               </li>
               <li className="social__item">
-                <a href="#!">
+                <a href="https://github.com/NikitaSipeykin" target="_blank" rel="noreferrer">
                   <img src={gitHub} alt="Link" />
                 </a>
               </li>
               <li className="social__item">
-                <a href="#!">
+                <a href="https://www.linkedin.com/in/nikita-sipeikin/" target="_blank" rel="noreferrer">
                   <img src={linkedIn} alt="Link" />
                 </a>
               </li>
