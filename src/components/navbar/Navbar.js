50cd61e80f4f72996dcb057cc0e4ebf276652ed7
MacNikSipeykin <niksipeykin@gmail.com>
2025-08-13 19:03:02 +0800

    Add dark mode functionality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,7 @@
-import sun from "./../../img/icons/sun.svg";
-import moon from "./../../img/icons/moon.svg";
+import { NavLink } from "react-router-dom";
+import BtnDarkMode from "../btnDarkMode/BtnDarkMode";
 import "./style.css";
 
-import { NavLink } from "react-router-dom";
 
 function Navbar() {
   const activeLink = "nav-list__link nav-list__link--active";
@@ -16,24 +15,36 @@ function Navbar() {
             <strong>Freelancer</strong> portfolio
           </NavLink>
 
-          <button className="dark-mode-btn">
-            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-          </button>
+          <BtnDarkMode />
 
           <ul className="nav-list">
             <li className="nav-list__item">
-              <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : inactiveLink)}>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? activeLink : inactiveLink
+                }
+              >
                 Home
               </NavLink>
             </li>
             <li className="nav-list__item">
-              <NavLink to="/projects" className={({ isActive }) => (isActive ? activeLink : inactiveLink)}>
+              <NavLink
+                to="/projects"
+                className={({ isActive }) =>
+                  isActive ? activeLink : inactiveLink
+                }
+              >
                 Projects
               </NavLink>
             </li>
             <li className="nav-list__item">
-              <NavLink to="/contacts" className={({ isActive }) => (isActive ? activeLink : inactiveLink)}>
+              <NavLink
+                to="/contacts"
+                className={({ isActive }) =>
+                  isActive ? activeLink : inactiveLink
+                }
+              >
                 Contacts
               </NavLink>
             </li>
